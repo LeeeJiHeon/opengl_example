56776b30f50294872c85d4180fd4738a36b68d81
LeeJiHeon <dlwlflgjs@naver.com>
2021-03-17 10:25:17 +0900

    사각형 만들기

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "context.h"
+
 #include <spdlog/spdlog.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -58,6 +60,13 @@ if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 auto glVersion = glGetString(GL_VERSION);
 SPDLOG_INFO("OpenGL context version: {}", glVersion);
 
+auto context = Context::Create();
+if (!context) {
+  SPDLOG_ERROR("failed to create context");
+  glfwTerminate();
+  return -1;
+}
+
 OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
 glfwSetFramebufferSizeCallback (window, OnFramebufferSizeChange);
 glfwSetKeyCallback(window, OnKeyEvent);
@@ -66,11 +75,10 @@ glfwSetKeyCallback(window, OnKeyEvent);
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
-        glClearColor(0.5f,0.5f,0.8f,0.6f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        context->Render();
         glfwSwapBuffers(window);
-
-    }
+}
+    context.reset();
 
     glfwTerminate();
     return 0;
