d73ae90fc398216ce566aef1561c6f5edb6f3ab2
LeeJiHeon <dlwlflgjs@naver.com>
2021-04-06 11:10:27 +0900

    Multiple Cube

--- src/program.cpp
+++ src/program.cpp
@@ -14,22 +14,32 @@ Program::~Program() {
 }
 
 bool Program::Link(const std::vector<ShaderPtr>& shaders) {
-  m_program = glCreateProgram();
-  for (auto& shader: shaders)
+    m_program = glCreateProgram();
+    for (auto& shader: shaders)
     glAttachShader(m_program, shader->Get());
-  glLinkProgram(m_program);
+    glLinkProgram(m_program);
 
-  int success = 0;
-  glGetProgramiv(m_program, GL_LINK_STATUS, &success);
-  if (!success) {
-    char infoLog[1024];
-    glGetProgramInfoLog(m_program, 1024, nullptr, infoLog);
-    SPDLOG_ERROR("failed to link program: {}", infoLog);
-    return false;
+    int success = 0;
+    glGetProgramiv(m_program, GL_LINK_STATUS, &success);
+    if (!success) {
+      char infoLog[1024];
+      glGetProgramInfoLog(m_program, 1024, nullptr, infoLog);
+      SPDLOG_ERROR("failed to link program: {}", infoLog);
+      return false;
   }
   return true;
 }
 
 void Program::Use() const {
-  glUseProgram(m_program);
+    glUseProgram(m_program);
+}
+
+void Program::SetUniform(const std::string& name, int value) const {
+    auto loc = glGetUniformLocation(m_program, name.c_str());
+    glUniform1i(loc, value);
+}
+
+void Program::SetUniform(const std::string& name, const glm::mat4& value) const {
+    auto loc = glGetUniformLocation(m_program, name.c_str());
+    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(value));
 }
