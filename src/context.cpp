b8513b05478e52b67a922cd4898ffa906ca13572
LeeJiHeon <dlwlflgjs@naver.com>
2021-05-04 15:05:52 +0900

    Spot Light

--- src/context.cpp
+++ src/context.cpp
@@ -211,6 +211,9 @@ void Context::Render() {
       }
       if (ImGui::CollapsingHeader("light", ImGuiTreeNodeFlags_DefaultOpen)) {
         ImGui::DragFloat3("l.position", glm::value_ptr(m_light.position), 0.01f);
+        ImGui::DragFloat3("l.direction", glm::value_ptr(m_light.direction), 0.01f);
+        ImGui::DragFloat2("l.cutoff", glm::value_ptr(m_light.cutoff), 0.5f, 0.0f, 180.0f);
+        ImGui::DragFloat("l.distance", &m_light.distance, 0.5f, 0.0f, 3000.0f);    
         ImGui::ColorEdit3("l.ambient", glm::value_ptr(m_light.ambient));
         ImGui::ColorEdit3("l.diffuse", glm::value_ptr(m_light.diffuse));
         ImGui::ColorEdit3("l.specular", glm::value_ptr(m_light.specular));
@@ -242,6 +245,9 @@ void Context::Render() {
       glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraYaw), glm::vec3(0.0f, 1.0f, 0.0f)) *
       glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraPitch), glm::vec3(1.0f, 0.0f, 0.0f)) *
       glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
+      
+    m_light.position = m_cameraPos;
+    m_light.direction = m_cameraFront;
     
     auto projection = glm::perspective(glm::radians(45.0f),
         (float)m_width / (float)m_height, 0.01f, 30.0f);
@@ -275,14 +281,14 @@ void Context::Render() {
     //     glm::vec3(0.0f, 0.0f, -3.0f));
 
     // after computing projection and view matrix
-    auto lightModelTransform =
-      glm::translate(glm::mat4(1.0), m_light.position) *
-      glm::scale(glm::mat4(1.0), glm::vec3(0.1f));
-    m_program->Use();
-    m_simpleProgram->Use();
-    m_simpleProgram->SetUniform("color", glm::vec4(m_light.ambient + m_light.diffuse, 1.0f));
-    m_simpleProgram->SetUniform("transform", projection * view * lightModelTransform);
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    // auto lightModelTransform =
+    //     glm::translate(glm::mat4(1.0), m_light.position) *
+    //     glm::scale(glm::mat4(1.0), glm::vec3(0.1f));
+    // m_program->Use();
+    // m_simpleProgram->Use();
+    // m_simpleProgram->SetUniform("color", glm::vec4(m_light.ambient + m_light.diffuse, 1.0f));
+    // m_simpleProgram->SetUniform("transform", projection * view * lightModelTransform);
+    // glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
    
     //빛의 위치
     // m_program->SetUniform("light.position", m_light.position);
@@ -295,6 +301,11 @@ void Context::Render() {
     m_program->Use();    	    	
     m_program->SetUniform("viewPos", m_cameraPos);
     m_program->SetUniform("light.position", m_light.position);
+    m_program->SetUniform("light.direction", m_light.direction);
+    m_program->SetUniform("light.cutoff", glm::vec2(
+        cosf(glm::radians(m_light.cutoff[0])),
+        cosf(glm::radians(m_light.cutoff[0] + m_light.cutoff[1]))));
+    m_program->SetUniform("light.attenuation", GetAttenuationCoeff(m_light.distance));
     m_program->SetUniform("light.ambient", m_light.ambient);
     m_program->SetUniform("light.diffuse", m_light.diffuse);
     m_program->SetUniform("light.specular", m_light.specular);
