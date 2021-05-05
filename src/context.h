b4d8f3d54c95822d9bc8441295e5aca9f9d1e0b2
LeeJiHeon <dlwlflgjs@naver.com>
2021-05-05 09:46:43 +0900

    Material Loading

--- src/context.h
+++ src/context.h
@@ -7,6 +7,8 @@
 #include "buffer.h"
 #include "vertex_layout.h"
 #include "texture.h"
+#include "mesh.h"
+#include "model.h"
 
 CLASS_PTR(Context)
 class Context {
@@ -24,11 +26,13 @@ private:
     ProgramUPtr m_program;
     ProgramUPtr m_simpleProgram;
 
-     VertexLayoutUPtr  m_vertexLayout;
-     BufferUPtr m_vertexBuffer;
-     BufferUPtr m_indexBuffer;
-     TextureUPtr m_texture;
-     TextureUPtr m_texture2;
+    //  VertexLayoutUPtr  m_vertexLayout;
+    //  BufferUPtr m_vertexBuffer;
+    //  BufferUPtr m_indexBuffer;
+    MeshUPtr m_box;
+    ModelUPtr m_model;
+    TextureUPtr m_texture;
+    TextureUPtr m_texture2;
      
      // animation
     bool m_animation { true };
@@ -47,6 +51,7 @@ private:
         glm::vec3 specular { glm::vec3(1.0f, 1.0f, 1.0f) };
     };
     Light m_light;
+    bool m_flashLightMode { false };
 
     // material parameter
     struct Material {
