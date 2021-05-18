867ee6f005f6ecdab6dfea9f677b24c7ceb33e18
LeeJiHeon <dlwlflgjs@naver.com>
2021-05-18 14:09:04 +0900

    Anti-Aliasing

--- src/context.cpp
+++ src/context.cpp
@@ -73,6 +73,7 @@ void Context::MouseButton(int button, int action, double x, double y) {
 }
 
 bool Context::Init() {
+    glEnable(GL_MULTISAMPLE);
     m_box = Mesh::CreateBox();
 
     // m_model = Model::Load("./model/backpack.obj");
@@ -189,6 +190,29 @@ bool Context::Init() {
     m_skyboxProgram = Program::Create("./shader/skybox.vs", "./shader/skybox.fs");
     m_envMapProgram = Program::Create("./shader/env_map.vs", "./shader/env_map.fs");
 
+    m_grassTexture = Texture::CreateFromImage(Image::Load("./image/grass.png").get());
+    m_grassProgram = Program::Create("./shader/grass.vs", "./shader/grass.fs");
+    m_grassPos.resize(10000);
+    for (size_t i = 0; i < m_grassPos.size(); i++) {
+        m_grassPos[i].x = ((float)rand() / (float)RAND_MAX * 2.0f - 1.0f) * 5.0f;
+        m_grassPos[i].z = ((float)rand() / (float)RAND_MAX * 2.0f - 1.0f) * 5.0f;
+        m_grassPos[i].y = glm::radians((float)rand() / (float)RAND_MAX * 360.0f);
+    }
+    	
+    m_grassInstance = VertexLayout::Create();
+    m_grassInstance->Bind();
+    m_plane->GetVertexBuffer()->Bind();
+    m_grassInstance->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+    m_grassInstance->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), offsetof(Vertex, normal));
+    m_grassInstance->SetAttrib(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), offsetof(Vertex, texCoord));
+    
+    m_grassPosBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW,
+        m_grassPos.data(), sizeof(glm::vec3), m_grassPos.size());
+    m_grassPosBuffer->Bind();
+    m_grassInstance->SetAttrib(3, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
+    glVertexAttribDivisor(3, 1);
+    m_plane->GetIndexBuffer()->Bind();
+
      return true;
 }
 
@@ -230,7 +254,7 @@ void Context::Render() {
   }
   ImGui::End();
 
-    m_framebuffer->Bind();
+    //m_framebuffer->Bind();
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
@@ -370,7 +394,7 @@ void Context::Render() {
     m_cubeTexture->Bind();
     m_envMapProgram->SetUniform("skybox", 0);
     m_box->Draw(m_envMapProgram.get());
-    
+
     // glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
     // glStencilMask(0x00);
     // glDisable(GL_DEPTH_TEST);
@@ -410,17 +434,30 @@ void Context::Render() {
     m_textureProgram->SetUniform("transform", transform);
     m_plane->Draw(m_textureProgram.get());
 
-    Framebuffer::BindToDefault();
+    glDisable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    m_grassProgram->Use();
+    m_grassProgram->SetUniform("tex", 0);
+    m_grassTexture->Bind();
+    m_grassInstance->Bind();
+    modelTransform = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.5f, 0.0f));
+    transform = projection * view * modelTransform;
+    m_grassProgram->SetUniform("transform", transform);
+    glDrawElementsInstanced(GL_TRIANGLES, m_plane->GetIndexBuffer()->GetCount(),
+        GL_UNSIGNED_INT, 0, m_grassPosBuffer->GetCount());
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    // Framebuffer::BindToDefault();
 
-    m_postProgram->Use();
-    m_postProgram->SetUniform("transform",
-        glm::scale(glm::mat4(1.0f), glm::vec3(2.0f, 2.0f, 1.0f)));
-    m_postProgram->SetUniform("gamma", m_gamma);
-    m_framebuffer->GetColorAttachment()->Bind();
-    m_postProgram->SetUniform("tex", 0);
-    m_plane->Draw(m_postProgram.get());
+    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    // m_postProgram->Use();
+    // m_postProgram->SetUniform("transform",
+    //     glm::scale(glm::mat4(1.0f), glm::vec3(2.0f, 2.0f, 1.0f)));
+    // m_postProgram->SetUniform("gamma", m_gamma);
+    // m_framebuffer->GetColorAttachment()->Bind();
+    // m_postProgram->SetUniform("tex", 0);
+    // m_plane->Draw(m_postProgram.get());
+
 //     glActiveTexture(GL_TEXTURE0);
 //     m_material.diffuse->Bind();
 //     glActiveTexture(GL_TEXTURE1);
